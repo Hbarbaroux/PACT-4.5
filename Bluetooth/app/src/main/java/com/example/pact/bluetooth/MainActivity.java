30387c0256088fcec2fe525fcc65d8dcf8ee059f
pewang <peng.wang@telecom-paristech.fr>
2016-12-12 11:16:25 +0100

    Bluetooth

---
+++ Bluetooth/app/src/main/java/com/example/pact/bluetooth/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.pact.bluetooth;
+
+
+import java.util.Set;
+import android.os.Bundle;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // TODO Auto-generated method stub
+
+            String action = intent.getAction();
+            // get bluetooth device searched 获得已经搜索到的蓝牙设备
+            if (action.equals(BluetoothDevice.ACTION_FOUND)) {
+                BluetoothDevice device = intent
+                        .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // search bluetooth device not connected before
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+                    // presente on screen 显示在TextView上
+                    mTextView.append(device.getName() + ":"
+                            + device.getAddress()+"\n");
+                }
+                // finish
+            } else if (action
+                    .equals(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)) {
+                setTitle("searching bluetooth device");
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.tvDevices);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        // get all device connected 获取所有已经绑定的蓝牙设备
+        Set<BluetoothDevice> devices = mBluetoothAdapter.getBondedDevices();
+        if (devices.size() > 0) {
+            for (BluetoothDevice bluetoothDevice : devices) {
+                mTextView.append(bluetoothDevice.getName() + ":"
+                        + bluetoothDevice.getAddress() + "\n\n");
+            }
+        }
+        IntentFilter mFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, mFilter);
+
+        mFilter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(mReceiver, mFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        //
+        unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void onClick_Search(View v) {
+
+        setTitle("being searching....");
+        // 如果正在搜索，就先取消搜索
+        if (mBluetoothAdapter.isDiscovering()) {
+            mBluetoothAdapter.cancelDiscovery();
+        }
+        // 开始搜索蓝牙设备,搜索到的蓝牙设备通过广播返回
+        mBluetoothAdapter.startDiscovery();
+    }
+
+}
