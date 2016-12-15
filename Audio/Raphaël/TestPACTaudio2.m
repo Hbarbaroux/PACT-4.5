ebbf0b8ddea32b5c61612e05eec8b0522902f8e8
Raphaël Teboul <raphael.teboul@telecom-paristech.fr>
2016-12-15 11:38:35 +0100

    frequence de l'attaque (qui marche)

--- Audio/Raphaël/TestPACTaudio2.m
+++ Audio/Raphaël/TestPACTaudio2.m
@@ -1,12 +1,20 @@
-Fs = 10000
+Fs = 10000;
 [y, Fs]=audioread('/cal/homes/rteboul/workspace/PACT-4.5/Audio/note.mp3'); % on charge le son enregistré
 T = 1/Fs ;
 L = 10000;
 t = (0:L-1)*T;
-Y = fft(y);
-denv = AttaqueHilbert(y,4410);
+f = (0:L-1)/L*Fs;
+NS= sin(880*pi*t).*(1-exp(-10*t));
+denv = AttaqueHilbert(NS,4410);
 M = max(denv);
 indiceDuMax = (find(denv==M));
-R = t(indiceDuMax)
-plot(t(1:L),denv(1:L))
+R = t(indiceDuMax);
+t1 = R - 0.1;
+t2 = R + 0.1;
+T1 = indiceDuMax ;
+T2 = indiceDuMax + 1000;
+Y = [zeros(1,T1-1),fft(NS(T1:T2)),zeros(1,length(NS)-T2)];
+Z = abs([zeros(1,999),Y(1000:length(Y))]);
+freq = f(find(Z==max(Z)))/10
+plot(f,Z);
 
