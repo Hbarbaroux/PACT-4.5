4dc36fa6eaa158037f145afd21c76180e2dc5aa0
Corentin LLORCA <corentin.llorca@telecom-paristech.fr>
2017-01-09 16:20:00 +0100

    retour à l'utilisation de derivLarge

--- Audio/Corentin/AttaqueEnveloppe.m
+++ Audio/Corentin/AttaqueEnveloppe.m
@@ -2,5 +2,5 @@ function derenv= AttaqueEnveloppe(signal,a)
 %après différents tests, la valeur de a la plus pertinente est 0.999
 n=length(signal);
 env=enveloppe(signal,a);
-derenv=derivLargeFilter(env,50)*n;
+derenv=derivLarge(env,441)*n;
 derenv=abs(derenv);
