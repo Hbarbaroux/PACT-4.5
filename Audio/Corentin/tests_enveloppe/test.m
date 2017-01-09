657cb98ea43bec4871dddade86bf8c88e8043f6e
Corentin LLORCA <corentin.llorca@telecom-paristech.fr>
2017-01-09 15:38:55 +0100

    ajout d'une mélodie à tester

--- Audio/Corentin/tests_enveloppe/test.m
+++ Audio/Corentin/tests_enveloppe/test.m
@@ -1,3 +1,3 @@
 function tst=test(y2,a)
 n=normalisation(enveloppe(y2,a));
-plot([8820:70272],normalisation(AttaqueEnveloppe(y2,a,4410)),[8820:70272],y2(8820:70272),[8820:70272],n(8820:70272));
+plot([1:70272],normalisation(AttaqueEnveloppe(y2,a)),[1:70272],n);
