5b2f2a1998d94bbd978bf54225907d57fe774ee3
Corentin LLORCA <corentin.llorca@telecom-paristech.fr>
2017-01-11 09:55:51 +0100

    débug afficheNotes

--- Audio/Corentin/ecouterNotes.m
+++ Audio/Corentin/ecouterNotes.m
@@ -1,10 +1,10 @@
 function audio = ecouterNotes(notes)
 %prend en entrée un tableau notes et nous permet de l'écouter
-audio=[]
+audio=[];
 n=length(notes)/2;
 for i=1:n-1
     l=notes(2*(i+1))-notes(2*i);
-    for j=1:l*44100
+    for j=1:floor(l*44100)
         audio=[audio,sin(j*2*pi*notes(2*i-1))];
     end
 end
