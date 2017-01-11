57e1c4ad8ff7c531dc3c89b823568384511a1756
Raphaël Teboul <raphael.teboul@telecom-paristech.fr>
2017-01-11 10:31:19 +0100

    correction attaqueEnveloppe

--- Audio/Corentin/AttaqueEnveloppe.m
+++ Audio/Corentin/AttaqueEnveloppe.m
@@ -2,5 +2,11 @@ function derenv= AttaqueEnveloppe(signal,a)
 %après différents tests, la valeur de a la plus pertinente est 0.999
 n=length(signal);
 env=enveloppe(signal,a);
-derenv=derivLarge(env,441)*n;
-derenv=abs(derenv);
+d=derivLarge(env,441)*n;
+derenv =[];
+for k=1:length(d)
+    derenv = [derenv, max([d(k), 0])]; %#ok<AGROW>
+end
+
+
+
--- Audio/Raphaël/test3.m
+++ Audio/Raphaël/test3.m
@@ -1,2 +1,2 @@
 q = audioread('/cal/homes/rteboul/workspace/PACT-4.5/Audio/test2.mp3');
-afficheNotes(notes(q));
+notes(q)
