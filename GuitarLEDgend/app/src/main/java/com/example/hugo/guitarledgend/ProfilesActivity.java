11b91fc458c7938c44f32f9b10288bdc1886f939
Hugo BARBAROUX <hugo.barbaroux@telecom-paristech.fr>
2017-01-11 23:06:25 +0100

    ProfilesActivity : ajout d'un bouton et d'un commentaire sur la non implémentation de la page

--- GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/ProfilesActivity.java
+++ GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/ProfilesActivity.java
@@ -1,7 +1,10 @@
 package com.example.hugo.guitarledgend;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ProfilesActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class ProfilesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profiles);
+
+        Button next_button = (Button) findViewById(R.id.next_button);
+
+        TextView comment = (TextView) findViewById(R.id.comment_profile);
+        Typeface century = Typeface.createFromAsset(getAssets(), "fonts/Century Gothic.ttf");
+        comment.setTypeface(century);
+        comment.setText("/* Page des profils non encore implémentée */ ");
+
+        TextView next_text_view = (TextView) findViewById(R.id.next_button);
+        Typeface century_bold = Typeface.createFromAsset(getAssets(), "fonts/Century Gothic Bold.ttf");
+        next_text_view.setTypeface(century_bold);
     }
 }
