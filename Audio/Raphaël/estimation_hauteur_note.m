101d525abdbacdf152dec60b98bfc672e2dce142
Raphaël Teboul <raphael.teboul@telecom-paristech.fr>
2017-01-11 11:34:56 +0100

    premier test audio

--- Audio/Raphaël/estimation_hauteur_note.m
+++ Audio/Raphaël/estimation_hauteur_note.m
@@ -5,12 +5,12 @@ Y=Y(1:floor(length(Y)/2));% transformée de fourier, on ne prend que la premièr
 Y=abs(Y);
 n = length(signal);
 Z=zeros(1, length(Y));
-for i=floor(100*44100/n):floor(2000*44100/n)
+for i=floor(100*44100/n):min([length(Y), floor(2000*44100/n)])
     Z(i)=Y(i);
 end
  % filtre passe-haut afin de se débarasser des effets de bords (la simple
  % concaténation renvoie une erreur...)
 f = (find(Z==max(Z)));
-freq = f*44100/(length(signal); %on divise par le nombre de secondes du signal, si on est à 44100 Hz de fréquence d'échantillonage
+freq = f*44100/(length(signal)); %on divise par le nombre de secondes du signal, si on est à 44100 Hz de fréquence d'échantillonage
 end
 
