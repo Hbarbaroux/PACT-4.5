3e9f801545f5455fb1ca4648f14636c27b580732
Hugo BARBAROUX <hugo.barbaroux@telecom-paristech.fr>
2017-01-11 22:48:18 +0100

    StartActivity : changement du ALL CAPITALS pour le texte des boutons, et ajout d'un intent vers ProfilesActivity pour le bouton Start.

--- GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/StartActivity.java
+++ GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/StartActivity.java
@@ -1,8 +1,11 @@
 package com.example.hugo.guitarledgend;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View.OnClickListener;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,7 +17,12 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         Button start_button = (Button) findViewById(R.id.start_button);
-
+        start_button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, ProfilesActivity.class);
+                startActivity(intent);
+            }
+        });
 
         TextView title = (TextView) findViewById(R.id.app_name);
         Typeface insomnia = Typeface.createFromAsset(getAssets(), "fonts/Android Insomnia Regular.ttf");
