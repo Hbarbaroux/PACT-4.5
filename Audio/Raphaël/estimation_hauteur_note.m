812d4389099098e3cec55b479d777ae769aa17ce
Raphaël Teboul <raphael.teboul@telecom-paristech.fr>
2017-01-11 11:16:25 +0100

    debug

--- Audio/Raphaël/estimation_hauteur_note.m
+++ Audio/Raphaël/estimation_hauteur_note.m
@@ -3,8 +3,9 @@ function freq = estimation_hauteur_note(signal)
 Y = fft(signal);
 Y=Y(1:floor(length(Y)/2));% transformée de fourier, on ne prend que la première moitié car elle est symétrique
 Y=abs(Y);
+n = length(signal);
 Z=zeros(1, length(Y));
-for i=1000:length(Y)
+for i=floor(100*44100/n):floor(2000*44100/n)
     Z(i)=Y(i);
 end
  % filtre passe-haut afin de se débarasser des effets de bords (la simple
