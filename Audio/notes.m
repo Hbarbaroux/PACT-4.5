eeb2e415d7b155ce1214c13b087b9dfc97919797
Raphaël Teboul <raphael.teboul@telecom-paristech.fr>
2017-01-11 09:26:57 +0100

    ajout d'un filtre passe-bas pour se débarasser du bruit
    affichage de la position des notes

--- Audio/notes.m
+++ Audio/notes.m
@@ -1,11 +1,12 @@
-function notes = notes(signal) % renvoie un tableau des positions temporelles de chaque note avec leur fréquence
-denv = AttaqueHilbert(signal,4410);
+function notes = notes(signal)% renvoie un tableau des positions temporelles de chaque note avec leur fréquence
+signalb = filter(1, [1, (1/10000)], signal);
+denv = AttaqueEnveloppe(signalb,0.999);
 Y = []; % index des montées
 L = []; % index des montées et des descentes autour de la valeur limite (max à 90%)
 notes = [];
 M = max(denv);
 i = 1;
-limite = 0.15
+limite = 0.3
     while i < length(denv) 
         while denv(i) < M*limite && i < length(denv)
             i = i + 1;
@@ -21,7 +22,7 @@ limite = 0.15
     end
     for k=1:(length(Y) - 1)
         q = signal((Y(k)):Y(k+1));
-        notes = [notes, estimation_hauteur_note(q)]; % notes contient dans les indices impairs les fréquences des notes et dans les indices pairs la position correspondante dans le morceau
+        notes = [notes, estimation_hauteur_note(q), Y(k)]; % notes contient dans les indices impairs les fréquences des notes et dans les indices pairs la position correspondante dans le morceau
     end
 end
     
