225e65eab9c235f1336956e7b02da3d2b81c2049
Hugo BARBAROUX <hugo.barbaroux@telecom-paristech.fr>
2017-01-11 22:28:02 +0100

    StartActivity : ajout du titre

--- GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/StartActivity.java
+++ GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/StartActivity.java
@@ -1,7 +1,9 @@
 package com.example.hugo.guitarledgend;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,6 +12,9 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        TextView title = (TextView) findViewById(R.id.app_name);
+        Typeface insomnia = Typeface.createFromAsset(getAssets(), "fonts/Android Insomnia Regular.ttf");
+        title.setTypeface(insomnia);
 
     }
 }
