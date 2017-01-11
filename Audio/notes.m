47b185696f8d2925951eb6ab499ea6f41b2e5bc4
Raphaël Teboul <raphael.teboul@telecom-paristech.fr>
2017-01-11 10:00:00 +0100

    affichage du temps en seconde

--- Audio/notes.m
+++ Audio/notes.m
@@ -23,7 +23,7 @@ limite = 0.3
     end
     for k=1:(length(Y) - 1)
         q = signal((Y(k)):Y(k+1));
-        notes = [notes, estimation_hauteur_note(q), Y(k)]; % notes contient dans les indices impairs les fréquences des notes et dans les indices pairs la position correspondante dans le morceau
+        notes = [notes, estimation_hauteur_note(q), Y(k)/44100]; % notes contient dans les indices impairs les fréquences des notes et dans les indices pairs la position correspondante dans le morceau
     end
 end
     
