e72236e98cf4b23d96c32f84582b2b611c2a19f6
Jesus Bujalance Martin <jesus.bujalance@telecom-paristech.fr>
2017-01-24 20:47:46 +0100

    ProfilesSQLiteHelper , PartitionsSQLiteHelper: création et début d'implémentation

---
+++ GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/PartitionsSQLiteHelper.java
@@ -0,0 +1,31 @@
+package com.example.hugo.guitarledgend;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+
+
+public class PartitionsSQLiteHelper extends SQLiteOpenHelper {
+
+    String sqlCreate = "CREATE TABLE Music (genre TEXT, auteur TEXT, nom TEXT, id TEXT)";
+
+    public PartitionsSQLiteHelper (Context contexto, String nombre, SQLiteDatabase.CursorFactory factory, int version) {
+        super(contexto, nombre, factory, version);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(sqlCreate);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL("DROP TABLE IF EXISTS Music");
+        db.execSQL(sqlCreate);
+    }
+
+
+
+}
