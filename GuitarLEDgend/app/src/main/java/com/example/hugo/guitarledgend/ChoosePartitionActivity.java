593244d785bfd58c7ed01c6cf3d951279c9e45ea
Jesus Bujalance Martin <jesus.bujalance@telecom-paristech.fr>
2017-01-24 21:08:05 +0100

    ChoosePartitionActivity: ajout des EditText

--- GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/ChoosePartitionActivity.java
+++ GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/ChoosePartitionActivity.java
@@ -1,10 +1,12 @@
 package com.example.hugo.guitarledgend;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class ChoosePartitionActivity extends AppCompatActivity {
 
@@ -16,10 +18,29 @@ public class ChoosePartitionActivity extends AppCompatActivity {
         Button ok = (Button) findViewById(R.id.ok_button_ChoosePartitionActivity);
         ok.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+
+                //recuperer le fichier grace au id et le transmettre a la nouvelle activite
+
                 Intent intent = new Intent(ChoosePartitionActivity.this, ChooseSpeedActivity.class);
                 startActivity(intent);
             }
         });
+
+        EditText editTextNom = (EditText) findViewById(R.id.nom_choosePartition);
+        String nom= editTextNom.getText().toString();
+
+        EditText editTextAuteur = (EditText) findViewById(R.id.auteur_choosePartition);
+        String auteur= editTextAuteur.getText().toString();
+
+        EditText editTextGenre = (EditText) findViewById(R.id.genre_choosePartition);
+        String genre= editTextGenre.getText().toString();
+
+
+        StatsSQLiteHelper ssqlh = new StatsSQLiteHelper(this,"MyMusic",null,1);
+        SQLiteDatabase db = ssqlh.getReadableDatabase();
+
+        String[] args = new String[] {genre,auteur,nom};
+        //Cursor c = db.rawQuery(" SELECT id FROM Music WHERE genre =? , auteur =? , nom =? ", args);
 
 
     }
