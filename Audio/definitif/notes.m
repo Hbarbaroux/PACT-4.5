f5e34596b8d6102aad320d1f0328a5f6d0ce5a7f
Corentin LLORCA <corentin.llorca@telecom-paristech.fr>
2017-01-26 16:49:57 +0100

    modifs notes.m (ne fonctionne pas ???)

--- Audio/definitif/notes.m
+++ Audio/definitif/notes.m
@@ -7,23 +7,26 @@ L = []; % index des montées et des descentes autour de la valeur limite (max à
 notes = [];
 M = max(denv);
 i = 1;
-limite = 0.1
-    while i < length(denv) 
-        while denv(i) < M*limite && i < length(denv)
-            i = i + 1;
-        end
-        L =  [L, i]; %#ok<*AGROW>
-        while denv(i) > M*limite && i < length(denv)
-            i = i + 1;
-        end
-        L = [L, i];
+limite = 0.1;
+while i < length(denv) 
+    while (denv(i) < M*limite && i < length(denv))
+        i = i + 1;
     end
-    for k=1:(length(L)/2)
-        Y = [Y, L(2*k-1)];
+    L =  [L, i]; %#ok<*AGROW>
+    while (denv(i) > M*limite && i < length(denv))
+        i = i + 1;
     end
-    for k=1:length(Y)
-        q = signalb((L(2*k-1)):L(2*k));
-        notes = [notes, estimation_hauteur_note(q), Y(k)]; % notes contient dans les indices impairs les fréquences des notes et dans les indices pairs la position correspondante dans le morceau
-    end
+    L = [L, i];
+end
+L=L
+for k=1:(length(L)/2)
+    Y = [Y, L(2*k-1)];
+end
+Y=Y
+for k=1:length(Y)
+    q = signalb(L(2*k-1):L(2*k));
+    notes = [notes, estimation_hauteur_note(q), Y(k)];
+    est=estimation_hauteur_note(q)% notes contient dans les indices impairs les fréquences des notes et dans les indices pairs la position correspondante dans le morceau
+end
 end
     
