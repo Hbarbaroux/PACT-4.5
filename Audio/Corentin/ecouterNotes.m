5bbc897f460c2af53c0a2ffa11d3348390c84a9e
Corentin LLORCA <corentin.llorca@telecom-paristech.fr>
2017-01-26 14:10:33 +0100

    ajout de testla (test la 440 pour audiowrite) et correction d'ecouterNotes

--- Audio/Corentin/ecouterNotes.m
+++ Audio/Corentin/ecouterNotes.m
@@ -5,11 +5,11 @@ n=length(notes)/2;
 for i=1:n-1
     l=notes(2*(i+1))-notes(2*i);
     for j=1:l
-        audio=[audio,sin(j*2*pi/notes(2*i-1))];
+        audio=[audio,sin(j*2*pi*notes(2*i-1)/44100)];
     end
 end
 for j=1:44100
-    audio=[audio,sin(j*2*pi/notes(2*n-1))];
+    audio=[audio,sin(j*2*pi*notes(2*n-1)/44100)];
 end
 
     
