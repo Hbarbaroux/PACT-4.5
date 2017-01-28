2b62a5da0c507232449c05cd31bc0448d670d890
Hugo BARBAROUX <hugo.barbaroux@telecom-paristech.fr>
2017-01-29 00:00:10 +0100

    MainActivity : suppression du bouton flottant de message

--- GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/Activities/MainActivity.java
+++ GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/Activities/MainActivity.java
@@ -27,15 +27,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         Button statsButton = (Button) findViewById(R.id.stats_button);
         statsButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
