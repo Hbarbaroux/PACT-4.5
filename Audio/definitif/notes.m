7cd06fae13b18068108a71b684d9b10e4d986c77
Raphaël Teboul <raphael.teboul@telecom-paristech.fr>
2017-01-30 09:31:02 +0100

    suppression doublon

--- Audio/definitif/notes.m
+++ Audio/definitif/notes.m
@@ -2,6 +2,7 @@
 function tabnotes = notes(signal)% renvoie un tableau des positions temporelles de chaque note avec leur fréquence
 signalb = filter(1, [1, (1/10000)], signal);% filtre passe bas se débarassant des fréquences trop aigues n'appartenant pas à la guitare
 denv = AttaqueEnveloppe(signalb,0.999); % partie positive de la dérivée de l'enveloppe 
+q = zeros(length(signalb))
 Y = []; % index des montées
 L = []; % index des montées et des descentes autour de la valeur limite (max à 90%)
 tabnotes = [];
@@ -22,7 +23,6 @@ end
 for k=1:(length(L)/2)
     Y = [Y, L(2*k-1)];
 end
-q = zeros(Y(length(Y)));
 for k=1:length(Y)
     q(L(2*k-1):L(2*k)) = signalb(L(2*k-1):L(2*k)) ; 
     tabnotes = [tabnotes, estimation_hauteur_note(q), Y(k)]; 
