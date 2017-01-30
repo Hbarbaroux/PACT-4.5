b2068a4ad1c09e7d7ce02b39a81dc0d0335882eb
Hugo BARBAROUX <hugo.barbaroux@telecom-paristech.fr>
2017-01-30 13:54:50 +0100

    ProfilesActivity : création de tabs

--- GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/activities/ProfilesActivity.java
+++ GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/activities/ProfilesActivity.java
@@ -3,6 +3,12 @@ package com.example.hugo.guitarledgend.activities;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,16 +16,20 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.hugo.guitarledgend.R;
 
+import java.util.ArrayList;
+
 public class ProfilesActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     public static String user = "USER EXAMPLE";
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +37,14 @@ public class ProfilesActivity extends AppCompatActivity implements NavigationVie
         setContentView(R.layout.activity_profiles);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
 
         Button next_button = (Button) findViewById(R.id.next_button);
         next_button.setOnClickListener(new View.OnClickListener() {
@@ -79,5 +97,104 @@ public class ProfilesActivity extends AppCompatActivity implements NavigationVie
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public static TabLayout tabLayout;
+        private SectionsPagerAdapter mSectionsPagerAdapter;
+
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+
+        public void removeTab(int position) {
+            if (tabLayout.getTabCount() >= 1 && position<tabLayout.getTabCount()) {
+                tabLayout.removeTabAt(position);
+                mSectionsPagerAdapter.removeTabPage(position);
+            }
+        }
+
+        private void addTab(String title) {
+            tabLayout.addTab(tabLayout.newTab().setText(title));
+            mSectionsPagerAdapter.addTabPage(title);
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        private final ArrayList<CharSequence> tabItems = new ArrayList<>();
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+            }
+            return null;
+        }
+
+        public void removeTabPage(int position) {
+            if (!tabItems.isEmpty() && position<tabItems.size()) {
+                tabItems.remove(position);
+                notifyDataSetChanged();
+            }
+        }
+
+        public void addTabPage(String title) {
+            tabItems.add(title);
+            notifyDataSetChanged();
+        }
     }
 }
