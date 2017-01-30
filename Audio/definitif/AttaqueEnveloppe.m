59322eca2e87f1064b43e09b6ff87a24b197c968
Corentin LLORCA <corentin.llorca@telecom-paristech.fr>
2017-01-30 18:02:15 +0100

    correction attaque enveloppe

--- Audio/definitif/AttaqueEnveloppe.m
+++ Audio/definitif/AttaqueEnveloppe.m
@@ -1,8 +1,8 @@
 function derenv= AttaqueEnveloppe(signal,a)
 %après différents tests, la valeur de a la plus pertinente est 0.999
 n=length(signal);
-env=enveloppe(log(signal),a);
-d=derivLarge(env,4410)*n;
+env=enveloppe(signal,a);
+d=derivLarge(env,4410);
 derenv =[];
 for k=1:length(d)
     derenv = [derenv, max([d(k), 0])]; %#ok<AGROW>
