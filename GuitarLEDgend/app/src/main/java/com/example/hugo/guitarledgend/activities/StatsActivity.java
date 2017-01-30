538b8f0739523ec37286a59487d26467b81b1daa
Jesus Bujalance Martin <jesus.bujalance@telecom-paristech.fr>
2017-01-30 16:57:30 +0100

    UserDAO: implementation de la fonction selectionnerProfil. StatsActivity: utilisation de cette fonction

--- GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/activities/StatsActivity.java
+++ GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/activities/StatsActivity.java
@@ -6,12 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 
 import com.example.hugo.guitarledgend.R;
 import com.example.hugo.guitarledgend.databases.partitions.Partition;
 import com.example.hugo.guitarledgend.databases.partitions.PartitionDAO;
+import com.example.hugo.guitarledgend.databases.users.Profile;
 import com.example.hugo.guitarledgend.databases.users.Stats;
 import com.example.hugo.guitarledgend.databases.users.UserDAO;
 import com.jjoe64.graphview.GraphView;
@@ -25,7 +25,7 @@ public class StatsActivity extends AppCompatActivity {
 
     public static final int DISPLAYED_STATS=10;
 
-    private UserDAO database;
+    private UserDAO database_user;
     private PartitionDAO database_partition;
     String partition_id;
 
@@ -47,18 +47,20 @@ public class StatsActivity extends AppCompatActivity {
         Intent intent =getIntent();
         long partition_id=intent.getLongExtra("partition_id",1L);
 
+        long profile_id = ProfilesActivity.getUser();
 
         database_partition = new PartitionDAO(StatsActivity.this);
         database_partition.open();
 
-        Partition partition = database_partition.selectionner(partition_id+1);
+        database_user = new UserDAO(StatsActivity.this);
+        database_user.open();
+
+        Partition partition = database_partition.selectionner(partition_id);
+        //Profile profil = database_user.selectionnerProfile(profile_id);
 
         //GRAPHE
 
-        database = new UserDAO(StatsActivity.this);
-        database.open();
-
-        List<Stats> values = database.getStats(ProfilesActivity.getUser(),partition_id+1);
+        List<Stats> values = database_user.getStats(profile_id,partition_id);
 
         DataPoint[] d1= new DataPoint[DISPLAYED_STATS];
         for (int i=0;i<Math.min(DISPLAYED_STATS,values.size());i++){
