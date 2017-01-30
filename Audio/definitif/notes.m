cfb331d78c3cf5d6860971f04b7d19810913257b
Corentin LLORCA <corentin.llorca@telecom-paristech.fr>
2017-01-30 08:59:48 +0100

    clarification de notes (nom du retourné = nom de la fonction)

--- Audio/definitif/notes.m
+++ Audio/definitif/notes.m
@@ -1,10 +1,10 @@
 
-function notes = notes(signal)% renvoie un tableau des positions temporelles de chaque note avec leur fréquence
+function tabnotes = notes(signal)% renvoie un tableau des positions temporelles de chaque note avec leur fréquence
 signalb = filter(1, [1, (1/10000)], signal);% filtre passe bas se débarassant des fréquences trop aigues n'appartenant pas à la guitare
 denv = AttaqueEnveloppe(signalb,0.999);
 Y = []; % index des montées
 L = []; % index des montées et des descentes autour de la valeur limite (max à 90%)
-notes = [];
+tabnotes = [];
 M = max(denv);
 i = 1;
 limite = 0.1;
@@ -25,7 +25,7 @@ end
 Y=Y
 for k=1:length(Y)
     q = signalb(L(2*k-1):L(2*k));
-    notes = [notes, estimation_hauteur_note(q), Y(k)];
+    tabnotes = [tabnotes, estimation_hauteur_note(q), Y(k)];
     est=estimation_hauteur_note(q)% notes contient dans les indices impairs les fréquences des notes et dans les indices pairs la position correspondante dans le morceau
 end
 end
