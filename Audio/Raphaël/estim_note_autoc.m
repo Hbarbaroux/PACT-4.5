8c84e1d847406501b9c8847bb343ff4c32f02d96
Raphaël Teboul <raphael.teboul@telecom-paristech.fr>
2017-02-06 09:26:37 +0100

    ajout d'un paramètre reglant la difficulté pour l'autocorrélation

--- Audio/Raphaël/estim_note_autoc.m
+++ Audio/Raphaël/estim_note_autoc.m
@@ -1,13 +1,13 @@
-function [ freq ] = estim_note_autoc(signal)
+function [ freq ] = estim_note_autoc(signal, NJ) % NJ correspond au niveau de justesse choisi par l'utilisateur: 0.7 = facile _ 0.8 = moyen _ 0.9 = difficile
 T = [];
 freq=0;
-j = 82.4;
+j = 155.55;
 while j < 1000;
     T = [T, abs(autoc(signal, 1/j))]; %#ok<AGROW>
     j = j*2^(1/12);
 end
-if max(T) > 0.7
+if max(T) > NJ
     z = findmax(T);
-    freq = 82.4*(2^(1/12)^(z+11));
+    freq = 155.55*(2^(1/12)^(z)); 
 end
 
