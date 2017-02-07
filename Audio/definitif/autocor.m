3c1da7bb64514e45f689dcade9a583059c135089
Raphaël Teboul <raphael.teboul@telecom-paristech.fr>
2017-02-07 11:39:11 +0100

    rectification

--- Audio/definitif/autocor.m
+++ Audio/definitif/autocor.m
@@ -1,10 +1,9 @@
 function t = autocor(signal, k)
-a = ifft(fft(signal(j))*fft(signal(j-floor(k*44100))));
 for j=1:(length(signal)-1)
     if j-k*44100 > 0
         b = b + signal(j)*signal(j);
         c = c + signal(j-floor(k*44100))*signal(j-floor(k*44100));
     end
 end
-t = a / sqrt(b*c);
+t = ifft(fft(signal(floor(k*44100):length(signal)))*fft(signal(0:(length(signal)-floor(k*44100))))) / sqrt(b*c);
 end
