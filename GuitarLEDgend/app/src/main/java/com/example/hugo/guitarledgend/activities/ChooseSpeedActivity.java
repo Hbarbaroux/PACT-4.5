5af2c112c706adde47c72fd1c0ba2a42217675cf
Jesus Bujalance Martin <jesus.bujalance@telecom-paristech.fr>
2017-02-07 11:35:00 +0100

    debut d'implementation de PostPlayingAcitivity. ChooseSpeedActivity: ajout des messages d'erreur

--- GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/activities/ChooseSpeedActivity.java
+++ GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/activities/ChooseSpeedActivity.java
@@ -3,8 +3,11 @@ package com.example.hugo.guitarledgend.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.hugo.guitarledgend.R;
 
@@ -20,14 +23,31 @@ public class ChooseSpeedActivity extends AppCompatActivity {
         Intent intent =getIntent();
         final long partition_id=intent.getLongExtra("partition_id",1L);
 
+        final EditText etSpeed = (EditText) findViewById(R.id.speed_text);
 
         Button ok = (Button) findViewById(R.id.ok_button_ChooseSpeedActivity);
         ok.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(ChooseSpeedActivity.this, PartitionPlayingActivity.class);
-                intent.putExtra("partition_id", partition_id);
-                startActivity(intent);
-                finish();
+
+                String vitesse = etSpeed.getText().toString();
+
+                if(TextUtils.isEmpty(vitesse)) {
+                    etSpeed.setError("Veuillez rentrer une vitesse");
+                }
+                else if(!TextUtils.isDigitsOnly(vitesse)){
+                    etSpeed.setError("Veuillez rentrer une vitesse coherente");
+                }
+                else if(((int) Integer.parseInt(vitesse)) <= 0){
+                    etSpeed.setError("Veuillez rentrer une vitesse positive");
+
+                }
+                else{
+                    Intent intent = new Intent(ChooseSpeedActivity.this, PartitionPlayingActivity.class);
+                    intent.putExtra("partition_id", partition_id);
+                    startActivity(intent);
+                    finish();
+                }
+
             }
         });
 
--- GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/activities/PostPlayingActivity.java
+++ GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/activities/PostPlayingActivity.java
@@ -1,12 +1,25 @@
 package com.example.hugo.guitarledgend.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.hugo.guitarledgend.R;
+import com.example.hugo.guitarledgend.databases.users.Stats;
+import com.example.hugo.guitarledgend.databases.users.UserDAO;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 public class PostPlayingActivity extends AppCompatActivity {
+
+    private UserDAO database;
 
     Intent intent =getIntent();
     final long partition_id=intent.getLongExtra("partition_id",1L);
@@ -17,6 +30,72 @@ public class PostPlayingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_playing);
 
+        fileCreation();
+        String file = newFile();
+
+        Date now = new Date();
+        String nowAsString = new SimpleDateFormat("yyyy-MM-dd").format(now);
+
+        Stats s= new Stats(0,nowAsString,file,score(file),partition_id,ProfilesActivity.getUser().getId());
+
+        database = new UserDAO(PostPlayingActivity.this);
+        database.open();
+        database.ajouter(s);
+
+        //GRAPHE
+
+        List<Integer> tab = s.tabFromFile(this);
+
+        DataPoint[] d= new DataPoint[tab.size()];
+        for (int i=0;i<tab.size();i++){
+
+            d[i]=new DataPoint(i, tab.get(i));
+        }
+
+
+        GraphView graph = (GraphView) findViewById(R.id.graph_PostPlayingActivity);
+
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(d);
+
+        series.setColor(R.color.colorPrimaryDark);
+        series.setDataWidth(1);
+        series.setSpacing(0);
+        series.setAnimated(true);
+
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(tab.size());
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(1.5);
+
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setXAxisBoundsManual(true);
+
+        graph.addSeries(series);
+
+        //graph.setTitle("DERNIERS SCORES : " + profil.getNom() + "/" + partition.getNom());
+        graph.setTitleColor(Color.BLACK);
+        graph.setTitleTextSize(100);
+
 
     }
+
+    //methode provisoire: cree un fichier faux des resultats aleatoirement dans le dossier reception
+    public void fileCreation(){
+
+    }
+
+    public String newFile(){
+        //va chercher le file dans reception et le renomme et deplace en data. retourne le nom du fichier
+        return null;
+    }
+
+    public int score(String file){
+        return 0;
+    }
+
+
+
+
+
+
 }
