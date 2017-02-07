14370ee0423e439ac227c5a8fc0a35fe0ad987b3
Raphaël Teboul <raphael.teboul@telecom-paristech.fr>
2017-02-07 12:08:17 +0100

    autoc qui marche pas

--- Audio/definitif/autocor.m
+++ Audio/definitif/autocor.m
@@ -7,5 +7,8 @@ for j=1:(length(signal)-1)
         c = c + signal(j-floor(k*44100))*signal(j-floor(k*44100));
     end
 end
-t = ifft(fft(signal(floor(k*44100):length(signal)))*fft(signal(0:(length(signal)-floor(k*44100))))) / sqrt(b*c);
+t = 0;
+if length(signal)-floor(k*44100)> 0
+    t = (ifft((fft(signal(floor(k*44100)+1:length(signal)))).*(fft(signal(1:length(signal)-floor(k*44100)))))) / sqrt(b*c);
+end
 end
