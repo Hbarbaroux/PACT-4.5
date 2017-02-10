3faa8b100c0a3d7f91974c6dbc65c521c9b9ed85
Hugo BARBAROUX <hugo.barbaroux@telecom-paristech.fr>
2017-02-10 17:05:10 +0100

    DeleteActivityFragment : création d'une boite de dialogue suggérant à l'utlisateur de supprimer une partition sélectionnée ChoosePartitionActivity : si la partition sélectionnée ne correspond plus à aucun fichier, invocation de la boite de dialogue précédente

--- GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/activities/ChoosePartitionActivity.java
+++ GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/activities/ChoosePartitionActivity.java
@@ -1,9 +1,11 @@
 package com.example.hugo.guitarledgend.activities;
 
+import android.support.v4.app.DialogFragment;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -17,10 +19,15 @@ import com.example.hugo.guitarledgend.R;
 import java.io.File;
 import java.util.List;
 
-public class ChoosePartitionActivity extends ListActivity {
+public class ChoosePartitionActivity extends FragmentActivity {
 
     private PartitionDAO database;
     ListView mListView;
+    private int position;
+
+    public int getPosition() {
+        return position;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,17 +50,24 @@ public class ChoosePartitionActivity extends ListActivity {
                 String fichier = database.selectionner(position + 1).getFichier();
                 File file = new File(Environment.getExternalStorageDirectory().getPath() + File.separator + "GuitarLEDgend" + File.separator + fichier);
 
+                ChoosePartitionActivity.this.position = position;
                 if (!(file.exists())) {
-                    Toast.makeText(ChoosePartitionActivity.this, "Le fichier correspondant n'existe plus", Toast.LENGTH_SHORT);
+                    askingForDeletion();
+                } else {
+                    Intent intent = new Intent(ChoosePartitionActivity.this, ChooseSpeedActivity.class);
+                    intent.putExtra("partition_id", (long) position + 1);
+                    startActivity(intent);
                 }
-
-                Intent intent = new Intent(ChoosePartitionActivity.this, ChooseSpeedActivity.class);
-                intent.putExtra("partition_id", (long) position+1);
-                startActivity(intent);
             }
         });
-
-
-
     }
+
+    public void askingForDeletion() {
+        DialogFragment newFragment = new DeleteActivityFragment();
+        newFragment.show(getSupportFragmentManager(), "delete");
+    }
+
+
+
+
 }
