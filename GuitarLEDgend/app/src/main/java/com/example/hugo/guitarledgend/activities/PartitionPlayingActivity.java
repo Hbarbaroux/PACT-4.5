de74dfcb6fa79762ffb211c3f9ef3c72dc2aeac6
Jesus Bujalance Martin <jesus.bujalance@telecom-paristech.fr>
2017-02-27 00:26:02 +0100

    PartitionPlayingActivity: le micro enregistre automatiquement pendant quon est sur cette activite

--- GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/activities/PartitionPlayingActivity.java
+++ GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/activities/PartitionPlayingActivity.java
@@ -26,115 +26,7 @@ public class PartitionPlayingActivity extends AppCompatActivity {
     private final int TEMPS = 5000;
 
     private Handler mHandler = new Handler();
-
-
-
     private static String mFileName = null;
-
-    private RecordButton mRecordButton = null;
-    private MediaRecorder mRecorder = null;
-
-    private PlayButton   mPlayButton = null;
-    private MediaPlayer mPlayer = null;
-
-    private void onRecord(boolean start) {
-        if (start) {
-            startRecording();
-        } else {
-            stopRecording();
-        }
-    }
-
-    private void onPlay(boolean start) {
-        if (start) {
-            startPlaying();
-        } else {
-            stopPlaying();
-        }
-    }
-
-    private void startPlaying() {
-        mPlayer = new MediaPlayer();
-        try {
-            mPlayer.setDataSource(mFileName);
-            mPlayer.prepare();
-            mPlayer.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void stopPlaying() {
-        mPlayer.release();
-        mPlayer = null;
-    }
-
-    private void startRecording() {
-        mRecorder = new MediaRecorder();
-        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mRecorder.setOutputFile(mFileName);
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-        try {
-            mRecorder.prepare();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        mRecorder.start();
-    }
-
-    private void stopRecording() {
-        mRecorder.stop();
-        mRecorder.release();
-        mRecorder = null;
-    }
-
-    class RecordButton extends Button {
-        boolean mStartRecording = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onRecord(mStartRecording);
-                if (mStartRecording) {
-                    setText("Stop recording");
-                } else {
-                    setText("Start recording");
-                }
-                mStartRecording = !mStartRecording;
-            }
-        };
-
-        public RecordButton(Context ctx) {
-            super(ctx);
-            setText("Start recording");
-            setOnClickListener(clicker);
-        }
-    }
-
-    class PlayButton extends Button {
-        boolean mStartPlaying = true;
-
-        OnClickListener clicker = new OnClickListener() {
-            public void onClick(View v) {
-                onPlay(mStartPlaying);
-                if (mStartPlaying) {
-                    setText("Stop playing");
-                } else {
-                    setText("Start playing");
-                }
-                mStartPlaying = !mStartPlaying;
-            }
-        };
-
-        public PlayButton(Context ctx) {
-            super(ctx);
-            setText("Start playing");
-            setOnClickListener(clicker);
-        }
-    }
-
 
     private long  partition_id=0;
     @Override
@@ -145,61 +37,39 @@ public class PartitionPlayingActivity extends AppCompatActivity {
         Intent intent =getIntent();
         partition_id=intent.getLongExtra("partition_id",1L);
 
+        File sdcard = Environment.getExternalStorageDirectory();
+        File dir = new File(sdcard.getPath()+"/GuitarLEDgend/audio/");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        File f = new File(dir,"audiorecordtest.3gp");
+        mFileName=f.getPath();
+
+        final MediaRecorder recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(mFileName);
+        try{
+            recorder.prepare();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        recorder.start();
+
 
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
+                recorder.stop();
+                recorder.release();
                 Intent intent = new Intent(PartitionPlayingActivity.this, PostPlayingActivity.class);
                 intent.putExtra("partition_id", partition_id);
                 startActivity(intent);
                 finish();            }
         },TEMPS);
 
-
-
-        File sdcard = Environment.getExternalStorageDirectory();
-
-        File dir = new File(sdcard.getPath()+"/GuitarLEDgend/audio/");
-        if (!dir.exists()) {
-            dir.mkdirs();
-        }
-
-        File f = new File(dir,"audiorecordtest.3gp");
-        mFileName=f.getPath();
-
-
-        LinearLayout ll = new LinearLayout(this);
-        mRecordButton = new RecordButton(this);
-        ll.addView(mRecordButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-        mPlayButton = new PlayButton(this);
-        ll.addView(mPlayButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-        setContentView(ll);
-
-
-
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mRecorder != null) {
-            mRecorder.release();
-            mRecorder = null;
-        }
-
-        if (mPlayer != null) {
-            mPlayer.release();
-            mPlayer = null;
-        }
-
+        
 
     }
 
