9924ae621a1b713532c32f29fb0ba512f368a081
Hugo BARBAROUX <hugo.barbaroux@telecom-paristech.fr>
2017-03-02 17:05:20 +0100

    HelpActivity : implémentation

--- GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/activities/ChoosePartitionActivity.java
+++ GuitarLEDgend/app/src/main/java/com/example/hugo/guitarledgend/activities/ChoosePartitionActivity.java
@@ -1,18 +1,15 @@
 package com.example.hugo.guitarledgend.activities;
 
 import android.support.v4.app.DialogFragment;
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
+import com.example.hugo.guitarledgend.bluetooth.TestBluetoothActivity;
 import com.example.hugo.guitarledgend.databases.partitions.Partition;
 import com.example.hugo.guitarledgend.databases.partitions.PartitionDAO;
 import com.example.hugo.guitarledgend.R;
