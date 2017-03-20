a4583dcfd3c01166911708cd8e66f88bd1cd5a02
cllorca <corentin.llorca@telecom-paristech.fr>
2017-03-20 09:55:32 +0100

    Ajout d'un package pour extraire un signal audio d'un fichier wav (à tester !!!!)

--- Audio/java/src/capture/Maintest.java
+++ Audio/java/src/capture/Maintest.java
@@ -4,9 +4,11 @@ public class Maintest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Float[] audio = Capture.wavcapture("testla.wav");
+		Float[] audio = Capture.wavcapture("data/testla.wav");
 		System.out.println(audio.length);
-		System.out.println(audio);
+		for (int i=0;i<audio.length; i=i+1000){
+			System.out.println(audio[i].floatValue());
+		}
 	}
 
 }
